4e44791699dd25185a9ee7bf31c7471dec4c7fec
Sagar <sagarlakhia18@gmail.com>
2019-09-29 17:26:39 -0700

    Add hello functionality

--- app/src/main/java/com/sagar/gitdemo/MainActivity.kt
+++ app/src/main/java/com/sagar/gitdemo/MainActivity.kt
@@ -2,11 +2,17 @@ package com.sagar.gitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        helloButton.setOnClickListener {
+            Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show()
+        }
     }
 }
